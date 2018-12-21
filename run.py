7ea846604b9b41d4b1ebec17b6d5819fc2dde0cf
Devavrat Tomar <devavrat694@gmail.com>
2018-12-22 00:58:34 +0100

    change default layers

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ output_path = './output_deep_cascade_nc4_nlayers3'
 data_provider_train = DataProvider(directory_name=train_data_path, epochs=1, file_extension='.npy')
 data_provider_test = DataProvider(directory_name=test_data_path, epochs=0, file_extension='.npy')
 
-deep_net = DeepCascade(x_channels=2, layers=2, ncascade=3, features=4, filter_size=3)
+deep_net = DeepCascade(x_channels=2, layers=3, ncascade=4, features=64, filter_size=3)
     
 trainer = Trainer(deep_net, batch_size=15, validation_batch_size=20)
 
